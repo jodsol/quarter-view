bdfcb4db6b7b2a3c798d51d994221ceed801bec1
hyehyeongjo2 <hyehyeong.jo@dabeeo.com>
2023-05-26 00:38:38 +0900

    feat: 점프 닷지 구현

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,17 +6,27 @@ public class Player : MonoBehaviour
 {
     public float speed;
 
+    int jumpPower = 15;
+
     float hAxis;
     float vAxis;
     bool wDown;
+    bool jDown;
+
+    bool isJump;
+    bool isDodge;
 
     Vector3 moveVec;
+    Vector3 dodgeVec;
+
+    Rigidbody rigid;
 
     Animator anim;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        rigid = GetComponent<Rigidbody>();
         anim = GetComponentInChildren<Animator>();
         
         
@@ -25,20 +35,84 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        GetInput();
+        Move();
+        Turn();
+        Jump(); 
+        Dodge();
+    }
+
+    void GetInput()
+    {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
         wDown = Input.GetButton("Walk");
+        jDown = Input.GetButton("Jump");
 
-        
+    }
+
+    void Move()
+    {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
-  
+        // dodge중 move못하게 수정
+        if(isDodge)
+        {
+            moveVec = dodgeVec;
+        }
+
 
         transform.position += moveVec * speed * (wDown ? 0.5f : 1f) * Time.deltaTime;
 
         anim.SetBool("isWalk", wDown);
         anim.SetBool("isRun", moveVec != Vector3.zero);
 
+    }
+
+    void Turn()
+    {
         transform.LookAt(transform.position + moveVec);
+    }
+
+    void Jump()
+    {
+        if (jDown && moveVec == Vector3.zero && !isJump && !isDodge)
+        {
+            rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            anim.SetBool("isJump", true);
+            anim.SetTrigger("doJump");
+            isJump = true;
+        }   
+    }
+
+    void Dodge()
+    {
+        if (jDown && moveVec != Vector3.zero && !isJump && !isDodge)
+        {
+            // 닷지중 방향이동 안됨
+            dodgeVec = moveVec;
+            rigid.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            speed *= 2;
+            anim.SetTrigger("doDodge");
+            isDodge = true;
+
+            Invoke("DodgeOut", 0.5f);
+        }
+    }
+
+    void DodgeOut()
+    {
+        speed *= 0.5f;
+        isDodge = false;
+    }
+
+    // 착지 구현
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            anim.SetBool("isJump", false);
+            isJump = false;
+        }
     }
 }
